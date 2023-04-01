47eadbcbbfd87ef91b91208b59bb75cfcbf327d2
afrangyan <afrangyan@kameleoon.com>
2023-04-01 18:51:47 +0300

    added skeleton-placeholders for loading

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import React from 'react';
 import Categories from './components/Categories';
 import Header from './components/Header';
 import PizzaBlock from './components/PizzaBlock';
+import Skeleton from './components/PizzaBlock/Skeleton';
 import Sort from './components/Sort';
 
 import './scss/app.scss';
 
 function App() {
   const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
     fetch('https://64283422161067a83b092b04.mockapi.io/items')
-      .then((res) => {
-        return res.json();
-      })
+      .then((res) => res.json())
       .then((json) => {
         setItems(json);
+        setIsLoading(false);
       });
   }, []);
 
@@ -31,9 +32,20 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {items.map((item) => (
-              <PizzaBlock key={item.id} {...item} />
-            ))}
+            {isLoading ? (
+              <>
+                <Skeleton />
+                <Skeleton />
+                <Skeleton />
+                <Skeleton />
+                <Skeleton />
+                <Skeleton />
+                <Skeleton />
+                <Skeleton />
+              </>
+            ) : (
+              items.map((item) => <PizzaBlock key={item.id} {...item} />)
+            )}
           </div>
         </div>
       </div>
---
+++ src/components/PizzaBlock/Skeleton.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ContentLoader from 'react-content-loader';
+
+const Skeleton = (props) => (
+  <ContentLoader
+    speed={2}
+    width={280}
+    height={465}
+    viewBox="0 0 345 465"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+    {...props}>
+    <circle cx="140" cy="130" r="130" />
+    <rect x="0" y="270" rx="10" ry="10" width="280" height="20" />
+    <rect x="0" y="310" rx="10" ry="10" width="280" height="88" />
+    <rect x="0" y="425" rx="10" ry="10" width="67" height="27" />
+    <rect x="127" y="415" rx="10" ry="10" width="152" height="45" />
+  </ContentLoader>
+);
+
+export default Skeleton;
