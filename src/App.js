0138eb47dffef9a44b790434e63679f7aa4beaed
afrangyan <afrangyan@kameleoon.com>
2023-04-01 20:55:44 +0300

    added routing

--- src/App.js
+++ src/App.js
@@ -1,52 +1,24 @@
 import React from 'react';
 
-import Categories from './components/Categories';
 import Header from './components/Header';
-import PizzaBlock from './components/PizzaBlock';
-import Skeleton from './components/PizzaBlock/Skeleton';
-import Sort from './components/Sort';
+import Home from './pages/Home';
+import { Routes, Route } from 'react-router-dom';
+import NotFound from './pages/NotFound.jsx';
+import Cart from './pages/Cart.jsx';
 
 import './scss/app.scss';
 
 function App() {
-  const [items, setItems] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(true);
-
-  React.useEffect(() => {
-    fetch('https://64283422161067a83b092b04.mockapi.io/items')
-      .then((res) => res.json())
-      .then((json) => {
-        setItems(json);
-        setIsLoading(false);
-      });
-  }, []);
-
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {isLoading ? (
-              <>
-                <Skeleton />
-                <Skeleton />
-                <Skeleton />
-                <Skeleton />
-                <Skeleton />
-                <Skeleton />
-                <Skeleton />
-                <Skeleton />
-              </>
-            ) : (
-              items.map((item) => <PizzaBlock key={item.id} {...item} />)
-            )}
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
+            <Route path="/cart" element={<Cart />}></Route>
+          </Routes>
         </div>
       </div>
     </div>
