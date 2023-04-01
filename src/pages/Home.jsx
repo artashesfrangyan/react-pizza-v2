b14702b705703f73fba3f63be1ad06ab2f89fb4f
afrangyan <afrangyan@kameleoon.com>
2023-04-01 22:57:07 +0300

    added some adaptivity, added cart page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,10 +15,11 @@ const Home = () => {
         setItems(json);
         setIsLoading(false);
       });
+    window.scrollTo(0, 0);
   }, []);
 
   return (
-    <>
+    <div className="container">
       <div className="content__top">
         <Categories />
         <Sort />
@@ -29,7 +30,7 @@ const Home = () => {
           ? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
           : items.map((item) => <PizzaBlock key={item.id} {...item} />)}
       </div>
-    </>
+    </div>
   );
 };
 
