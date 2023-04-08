5625276e61999b8c18042a06b28b92d973f6dc0a
afrangyan <afrangyan@kameleoon.com>
2023-04-08 14:17:21 +0300

    added Outlet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import Header from './components/Header';
 import Home from './pages/Home';
 import { Routes, Route } from 'react-router-dom';
 import NotFound from './pages/NotFound.jsx';
@@ -8,20 +7,18 @@ import Cart from './pages/Cart.jsx';
 
 import './scss/app.scss';
 import FullPizza from './pages/FullPizza';
+import MainLayout from './layouts/MainLayout';
 
 function App() {
   return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="*" element={<NotFound />}></Route>
-          <Route path="/pizza/:id" element={<FullPizza />}></Route>
-          <Route path="/cart" element={<Cart />}></Route>
-        </Routes>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="" element={<Home />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+        <Route path="pizza/:id" element={<FullPizza />}></Route>
+        <Route path="cart" element={<Cart />}></Route>
+      </Route>
+    </Routes>
   );
 }
 
