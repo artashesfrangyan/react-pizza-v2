8352fc18aa722e1859bfc649ac983c78a75b5240
afrangyan <afrangyan@kameleoon.com>
2023-04-15 21:55:37 +0300

    added reexport

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import Categories from '../components/Categories';
+import PizzaBlock from '../components/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+import Sort, { options } from '../components/Sort';
+import Pagination from '../components/Pagination';
+
+export { Categories, PizzaBlock, Skeleton, Sort, options, Pagination };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,7 @@ import { useSelector } from 'react-redux';
 import qs from 'qs';
 import { useNavigate } from 'react-router-dom';
 
-import Categories from '../components/Categories';
-import PizzaBlock from '../components/PizzaBlock';
-import Skeleton from '../components/PizzaBlock/Skeleton';
-import Sort, { options } from '../components/Sort';
-import Pagination from '../components/Pagination';
+import { Categories, PizzaBlock, Skeleton, Sort, options, Pagination } from '../components';
 
 import {
   setCategoryId,
